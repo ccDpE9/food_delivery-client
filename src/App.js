26b7ab761e43c31b1ee906747fbd1aaa5cabbc0f
ccDpE9 <z_eon00@protonmail.com>
2020-10-06 18:56:02 +0200

    App component is simply defines all the application's routes.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Restaurants from "./components/restaurants/Restaurants";
+import Restaurant from "./components/restaurants/Restaurant";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Navigation from "./components/navigation/Navigation";
@@ -21,6 +22,10 @@ function App() {
           <Route path="/register">
             <Register />
           </Route>
+          <Route
+            path="/restaurant/:slug"
+            render={(prop) => <Restaurant slug={prop} />}
+          />
         </Switch>
       </Router>
     </div>
